b29b75c66b62849a4c170f66b79a69d038e54b9a
Aqustor <86988706+Aqustor@users.noreply.github.com>
2022-12-20 20:34:45 +0100

    Update CircleApp.py

--- CircleApp.py
+++ CircleApp.py
@@ -1,68 +1,97 @@
+import sys
+
 import pygame
 import random
 
-# Inicjalizacja biblioteki pygame
+# Inicjalizacja pygame
 pygame.init()
 
 # Ustawienie wielkości okna
-screen_width, screen_height = 800, 800
-screen = pygame.display.set_mode((screen_width, screen_height))
+window_size = (1200, 800)
 
-# Ustawienie tła okna na kolor ciemnoszary
-screen.fill((60, 60, 60))
+# Ustawienie tła
+background_color = (50, 50, 50)
 
-# Ustawienie tytułu okna
-pygame.display.set_caption('Kółka')
+# Ustawienie kolorów kółek
+circle_colors = [(255, 0, 0), (0, 255, 0)]
 
-# Ustawienie promienia kółek na daną wielkość pikseli
-radius = 10
+# Ustawienie ilości kulek czerwonych i zielonych
+num_red_circles = 15
+num_green_circles = 15
 
+# Ustawienie szybkości poruszania się kulek
+circle_speed = 10
 
-# Klasa reprezentująca kółka
-class Circle:
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.dx = 1  # Prędkość w poziomie
-        self.dy = 1  # Prędkość w pionie
+# Utworzenie okna
+screen = pygame.display.set_mode(window_size)
 
-    def move(self):
-        self.x += self.dx
-        self.y += self.dy
+# Utworzenie zegara, który będzie używany do opóźniania wyświetlania klatek
+clock = pygame.time.Clock()
 
-        # Odbicie od ścianek
-        if self.x + radius > screen_width or self.x - radius < 0:
-            self.dx *= -1
-        if self.y + radius > screen_height or self.y - radius < 0:
-            self.dy *= -1
+# Utworzenie pustej listy, w której będą przechowywane kółka
+circles = []
 
-    def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), radius)
+# Utworzenie kółek czerwonych
+for i in range(num_red_circles):
+  # Losowanie położenia początkowego kółka
+  x = random.randint(0, window_size[0])
+  y = random.randint(0, window_size[1])
+  # Utworzenie kółka z odpowiednimi parametrami
+  circle = {"color": circle_colors[0], "radius": 25, "position": (x, y), "direction": (circle_speed, circle_speed)}
+  # Dodanie kółka do listy
+  circles.append(circle)
 
-
-# Losowe położenie kółek na starcie
-circle1 = Circle(random.randint(radius, screen_width - radius), random.randint(radius, screen_height - radius),
-                 (255, 0, 0))
-circle2 = Circle(random.randint(radius, screen_width - radius), random.randint(radius, screen_height - radius),
-                 (0, 255, 0))
+# Utworzenie kółek zielonych
+for i in range(num_green_circles):
+  # Losowanie położenia początkowego kółka
+  x = random.randint(0, window_size[0])
+  y = random.randint(0, window_size[1])
+  # Utworzenie kółka z odpowiednimi parametrami
+  circle = {"color": circle_colors[1], "radius": 25, "position": (x, y), "direction": (circle_speed, circle_speed)}
+  # Dodanie kółka do listy
+  circles.append(circle)
 
 # Główna pętla programu
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+while True:
+  # Ustawienie ilości klatek na sekundę
+  clock.tick(60)
 
-    # Ruch kółek
-    circle1.move()
-    circle2.move()
+  # Pętla zdarzeń
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
 
-    # Wyświetlenie kółek na ekranie
-    screen.fill((60, 60, 60))
-    circle1.draw()
-    circle2.draw()
-    pygame.display.flip()
+  # Aktualizacja położenia kółek
+  for circle in circles:
+    # Pobranie aktualnego położenia kółka
+    x, y = circle["position"]
+    # Pobranie kierunku ruchu kółka
+    dx, dy = circle["direction"]
+    # Aktualizacja położenia kółka
+    x += dx
+    y += dy
+    # Sprawdzenie, czy kółko nie wyszło poza granice okna
+    if x < 0 or x > window_size[0]:
+      dx = -dx
+    if y < 0 or y > window_size[1]:
+      dy = -dy
+    # Uaktualnienie kierunku ruchu kółka
+    circle["direction"] = (dx, dy)
+    # Uaktualnienie położenia kółka
+    circle["position"] = (x, y)
 
-# Zakończenie biblioteki pygame
-pygame.quit()
+  # Wyświetlenie tła
+  screen.fill(background_color)
+
+  # Wyświetlenie kółek
+  for circle in circles:
+    # Pobranie koloru i położenia kółka
+    color = circle["color"]
+    x, y = circle["position"]
+    radius = circle["radius"]
+    # Utworzenie obiektu reprezentującego kółko
+    pygame.draw.circle(screen, color, (x, y), radius)
+
+  # Wyświetlenie zmian na ekranie
+  pygame.display.flip()
